e3333a7ce626527032a03c61724bcaf5af7c1a13
Diego Lamarão <diego.lamarao92@gmail.com>
2019-04-10 10:34:09 -0300

    v1.0.0

---
+++ app.js
@@ -0,0 +1,137 @@
+function mongoConnect (MongoClient) {
+  return new Promise((resolve, reject) => {
+    MongoClient.connect('mongodb://localhost:27017/clubeextra', { useNewUrlParser: true }, function(err, client) {
+      if(err) reject(err)
+      else resolve(client.db('clubeextra'))
+    })
+  })
+}
+
+module.exports = class {
+  constructor ({ axios, categories, MongoClient, formatProductUrl }) {
+    this.axios = axios
+    this.categories = categories
+    this.db = mongoConnect(MongoClient)
+    this.formatProductUrl = formatProductUrl
+  }
+
+  async init () {
+    try {
+      const app = this
+      app.db = await app.db
+
+      const cursor = app.db.collection('products')
+      cursor.createIndex({ id: 1 }, { unique: true })
+
+      const count = await cursor.find().count()
+
+      if (!count) await populateDatabase()
+
+      const allProductsIds = await cursor.find({}).toArray()
+
+      const allProductsDetails = await allProductsIds.reduce(async (acc, { id }) => {
+        acc = await acc
+
+        const productDetails = await app.getProductDetail(id)
+        acc.push(productDetails)
+        console.log(`Requisitando detalhes dos produtos: ${acc.length} de ${count}`)
+
+        return acc
+      }, [])
+
+      await app.setProductsToDB(allProductsDetails)
+
+    } catch (err) {
+      console.error('ERROR: ', err)
+    }
+    console.log('END')
+  }
+
+  async getProductDetail (productId) {
+    const { data } = await this.axios(this.formatProductUrl(productId))
+    await this.waitTime(100)
+    return data.content
+  }
+
+  async populateDatabase () {
+    const app = this
+    return await app.categories.reduce(async (next, { name, subCategory }) => {
+      next = await next
+      console.log(`Request Categoria: ${name}`)
+
+      const products = await subCategory.reduce(async (list, subcategorie) => {
+        list = await list
+
+        const products = await app.requestAllProductsFromCategorieUrl(subcategorie)
+
+        return [ ...list, ...products]
+      }, [])
+
+      await app.setProductsToDB(products)
+
+      return next
+    }, true)
+  }
+
+  async requestAllProductsFromCategorieUrl ({ name, link }) {
+    console.log(` - Request Subcategoria: ${name}`)
+    var page = 0
+    var allProducts = []
+
+    const { totalPages, products } =  await this.requestProductsbyPage(link, page)
+
+    allProducts = products
+    page++
+
+    while (page < totalPages) {
+
+      const { products } =  await this.requestProductsbyPage(link, page)
+      allProducts = [ ...allProducts, ...products ]
+      page++
+
+    }
+    return allProducts
+  }
+
+  async requestProductsbyPage (url, page) {
+    console.log(`Página: ${page + 1}`)
+    url = url.replace('?p=', '?p=' + page)
+
+    const { data } = await this.axios(url)
+    await this.waitTime(100)
+
+    const { products, totalPages } = data.content
+    return {
+      products: products.map(({ id, urlDetails }) => {
+        return { id, urlDetails }
+      }),
+      totalPages
+    }
+  }
+
+  async setProductsToDB (products) {
+    return await this.db.collection('products').bulkWrite(
+      products.map(product => {
+        return {
+          updateOne: {
+            filter : {
+              id: product.id
+            },
+            update : {
+              $set: product
+            },
+            upsert : true,
+          }
+        }
+      })
+    )
+  }
+
+  waitTime (time) {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve(true)
+      }, time)
+    })
+  }
+}
---
+++ index.js
@@ -0,0 +1,19 @@
+const App = require('./app')
+
+const axiosOptions = {
+  headers: {
+    'Accept': 'application/json, text/plain, */*',
+    'betaFeatures': 'true',
+    'Origin': 'https://www.clubeextra.com.br',
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36'
+  }
+}
+
+const dependencies = {
+  MongoClient: require("mongodb").MongoClient,
+  axios: require('axios').create(axiosOptions),
+  categories: require('./categories').categories,
+  formatProductUrl: require('./format-product-url')
+}
+
+new App(dependencies).init()
